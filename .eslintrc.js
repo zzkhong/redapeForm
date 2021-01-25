a8df3280314ab7007f06ed3e678c9963a16e9220
zzkhong <khongc7@gmail.com>
2021-01-25 20:54:02 +0800

    Setup: Setup linting, babel and pre-commit hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,50 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    // Airbnb linting issues
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'no-use-before-define': 0,
+    'react/display-name': 0,
+    'react/prop-types': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+    'no-empty-pattern': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    'no-empty': 0,
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          common: './src/common',
+          modules: './src/modules',
+          ui: './src/ui',
+        },
+      },
+    ],
+  ],
 };
